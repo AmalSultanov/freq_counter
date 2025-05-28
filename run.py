b2263b2eb70c7630bef98af09afa4fc8a2f28a51
AmalSultanov <pogboom1014@gmail.com>
2025-05-28 17:49:41 +0500

    app.py => run.py as entry point

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from app import create_app
+from app.config import flask_port, flask_debug
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=flask_port, debug=flask_debug)
