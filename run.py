86bda6432ce2e10963017c6cdf35ba1858936704
AmalSultanov <pogboom1014@gmail.com>
2025-05-30 00:24:38 +0500

    Remove redundant args in run()

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 from app import create_app
-from app.config import flask_port, flask_debug
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(port=flask_port, debug=flask_debug)
+    app.run()
