200ce94d4f00bc1ca97be74ca522ea62813abcc6
AmalSultanov <pogboom1014@gmail.com>
2025-06-09 23:58:03 +0500

    Create model for storing collection related data and services over it

---
+++ app/collections/models.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+from app.database import db
+
+
+class CollectionModel(db.Model):
+    __tablename__ = "collections"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(256), nullable=False)
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey("users.id", ondelete="CASCADE"),
+        nullable=False
+    )
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    documents = db.relationship(
+        "DocumentCollectionModel",
+        back_populates="collection"
+    )
+
+    def __repr__(self):
+        return f"<CollectionModel {self.id}: {self.name}>"
+
+    def __str__(self):
+        return self.name
