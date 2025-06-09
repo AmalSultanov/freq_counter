6e3f16a176b4f1468de577652462790e8287d516
AmalSultanov <pogboom1014@gmail.com>
2025-06-10 00:12:14 +0500

    Set JWT related variables in the config class

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 from dotenv import load_dotenv
 
@@ -7,6 +8,20 @@ load_dotenv()
 flask_port = os.getenv("FLASK_PORT")
 flask_debug = os.getenv("FLASK_DEBUG")
 flask_env = os.getenv("FLASK_ENV")
+
+jwt_secret_key = os.getenv("JWT_SECRET_KEY")
+jwt_token_location = os.getenv("JWT_TOKEN_LOCATION")
+jwt_access_cookie_path = os.getenv("JWT_ACCESS_COOKIE_PATH")
+jwt_refresh_cookie_path = f"{os.getenv('JWT_REFRESH_COOKIE_PATH')}"
+jwt_cookie_csrf_protect = os.getenv("JWT_COOKIE_CSRF_PROTECT")
+jwt_cookie_secure = os.getenv("JWT_COOKIE_SECURE")
+jwt_access_token_expires_minutes = int(os.getenv(
+    "JWT_ACCESS_TOKEN_EXPIRES_MINUTES"
+))
+jwt_refresh_token_expires_days = int(os.getenv(
+    "JWT_REFRESH_TOKEN_EXPIRES_DAYS"
+))
+
 postgres_user = os.getenv("POSTGRES_USER")
 postgres_password = os.getenv("POSTGRES_PASSWORD")
 postgres_host = os.getenv("POSTGRES_HOST")
@@ -22,6 +37,18 @@ class Config:
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MEDIA_FOLDER = os.path.join(os.path.dirname(__file__), "media")
+    JWT_SECRET_KEY = jwt_secret_key
+    JWT_TOKEN_LOCATION = jwt_token_location
+    JWT_ACCESS_COOKIE_PATH = jwt_access_cookie_path
+    JWT_REFRESH_COOKIE_PATH = jwt_refresh_cookie_path
+    JWT_COOKIE_CSRF_PROTECT = jwt_cookie_csrf_protect
+    JWT_COOKIE_SECURE = jwt_cookie_secure
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(
+        minutes=jwt_access_token_expires_minutes
+    )
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(
+        days=jwt_refresh_token_expires_days
+    )
 
 
 class DevelopmentConfig(Config):
