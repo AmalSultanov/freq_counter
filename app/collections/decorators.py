e57804b621110c05054ff49cdbfd362596c3274b
AmalSultanov <pogboom1014@gmail.com>
2025-06-11 19:14:49 +0500

    Tune decorators to work with resources

--- app/collections/decorators.py
+++ app/collections/decorators.py
@@ -1,6 +1,5 @@
 from functools import wraps
 
-from flask import jsonify
 from flask_jwt_extended import get_jwt_identity
 
 from app.collections.services import (
@@ -11,53 +10,54 @@ from app.users.services import get_user_by_username
 
 def ensure_user_collection_exists(func):
     @wraps(func)
-    def wrapper(collection_id, *args, **kwargs):
+    def wrapper(*args, **kwargs):
+        collection_id = kwargs.get("collection_id")
         username = get_jwt_identity()
         user = get_user_by_username(username)
 
         if not user_collection_exists(collection_id, user):
-            return jsonify({
-                "message": "This user does not have such collection"
-            }), 404
-        return func(collection_id, *args, **kwargs)
-
+            return {"message": "This user does not have such collection"}, 404
+        return func(*args, **kwargs)
     return wrapper
 
 
 def ensure_user_document_exists(func):
     @wraps(func)
-    def wrapper(collection_id, document_id, *args, **kwargs):
+    def wrapper(*args, **kwargs):
+        document_id = kwargs.get("document_id")
         username = get_jwt_identity()
         user = get_user_by_username(username)
 
         if not user_document_exists(document_id, user):
-            return jsonify({
-                "message": "This user does not have such document"
-            }), 404
-        return func(collection_id, document_id, *args, **kwargs)
-
+            return {"message": "This user does not have such document"}, 404
+        return func(*args, **kwargs)
     return wrapper
 
 
 def ensure_document_not_in_collection(func):
     @wraps(func)
-    def wrapper(collection_id, document_id, *args, **kwargs):
-        if document_in_collection_exists(collection_id, document_id):
-            return jsonify({
-                "message": "This document already exists in this collection"
-            }), 409
-        return func(collection_id, document_id, *args, **kwargs)
+    def wrapper(*args, **kwargs):
+        collection_id = kwargs.get("collection_id")
+        document_id = kwargs.get("document_id")
 
+        if document_in_collection_exists(collection_id, document_id):
+            return {
+                "message": "This document already exists in this collection"
+            }, 409
+        return func(*args, **kwargs)
     return wrapper
 
 
 def ensure_document_in_collection(func):
     @wraps(func)
-    def wrapper(collection_id, document_id, *args, **kwargs):
+    def wrapper(*args, **kwargs):
+        collection_id = kwargs.get("collection_id")
+        document_id = kwargs.get("document_id")
+
         if not document_in_collection_exists(collection_id, document_id):
-            return jsonify({
+            return {
                 "message": "This document is not part of this collection"
-            }), 409
-        return func(collection_id, document_id, *args, **kwargs)
+            }, 409
+        return func(*args, **kwargs)
 
     return wrapper
