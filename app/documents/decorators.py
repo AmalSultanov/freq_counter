51027b20952a8551cb056ae697d2e4b9ac3b3972
AmalSultanov <pogboom1014@gmail.com>
2025-06-11 19:17:39 +0500

    Tune decorators and error handlers to work with resources

--- app/documents/decorators.py
+++ app/documents/decorators.py
@@ -1,24 +1,19 @@
 from functools import wraps
 
-from flask import jsonify
 from flask_jwt_extended import get_jwt_identity
 
-from app.documents.services import (
-    user_has_document
-)
+from app.documents.services import user_has_document
 from app.users.services import get_user_by_username
 
 
 def ensure_user_document_exists(func):
     @wraps(func)
-    def wrapper(document_id, *args, **kwargs):
+    def wrapper(*args, **kwargs):
+        document_id = kwargs.get("document_id")
         username = get_jwt_identity()
         user = get_user_by_username(username)
 
         if not user_has_document(document_id, user):
-            return jsonify({
-                "message": "This user does not have such document"
-            }), 404
-        return func(document_id, *args, **kwargs)
-
+            return {"message": "This user does not have such document"}, 404
+        return func(*args, **kwargs)
     return wrapper
--- app/documents/error_handlers.py
+++ app/documents/error_handlers.py
@@ -1,13 +1,20 @@
-from flask import jsonify
+from flask_jwt_extended.exceptions import (
+    NoAuthorizationError, InvalidHeaderError
+)
 
 from app.shared.exceptions import DuplicateDocumentError, EmptyFileError
 
 
-def register_documents_errors_handlers(bp):
-    @bp.errorhandler(DuplicateDocumentError)
-    def handle_duplicate_document(error):
-        return jsonify({"message": str(error)}), 409
+def register_documents_errors_handlers(api):
+    @api.errorhandler(NoAuthorizationError)
+    @api.errorhandler(InvalidHeaderError)
+    def handle_auth_error(error):
+        return {"message": str(error)}, 401
 
-    @bp.errorhandler(EmptyFileError)
+    @api.errorhandler(DuplicateDocumentError)
+    def handle_duplicate_document(error):
+        return {"message": str(error)}, 409
+
+    @api.errorhandler(EmptyFileError)
     def handle_empty_document(error):
-        return jsonify({"message": str(error)}), 400
+        return {"message": str(error)}, 400
