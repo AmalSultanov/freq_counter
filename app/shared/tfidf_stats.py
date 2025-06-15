c5777323e8af68c184cd273a6a342614296ca94a
AmalSultanov <pogboom1014@gmail.com>
2025-06-15 20:49:57 +0500

    Add helper functions for stats calculating

--- app/shared/tfidf_stats.py
+++ app/shared/tfidf_stats.py
@@ -4,11 +4,28 @@ import math
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
+def get_word_stats(text: str) -> tuple[Counter[str], int]:
+    tokens = tokenize_text(text)
+    total_words = len(tokens)
+    word_counts = Counter(tokens)
+
+    return word_counts, total_words
+
+
 def tokenize_text(text: str) -> list[str]:
     vectorizer = TfidfVectorizer(use_idf=False, norm=None)
     analyzer = vectorizer.build_analyzer()
 
     return analyzer(text)
+
+
+def get_document_tf(document_contents: str) -> dict[str, float] | None:
+    word_counts, total_words = get_word_stats(document_contents)
+    tf_dict = calculate_tf(word_counts, total_words)
+    least_frequent_words = get_least_frequent_words(tf_dict)
+    tf = {word: tf_dict[word] for word in least_frequent_words}
+
+    return tf
 
 
 def calculate_tf(word_counts: Counter, total_words: int) -> dict[str, float]:
